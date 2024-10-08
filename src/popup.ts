d7c6165e0cf0bc8dcb944be511f363d77e1b7f79
Emilio Aranda <ara.emi44@gmail.com>
2024-10-09 00:17:15 +0200

    added infinite loop.

--- src/popup.ts
+++ src/popup.ts
@@ -17,18 +17,24 @@ deleteAll.onclick = async function(event) {
   const bgozhURL = new URL("https://bgozh.hotelkit.net");
   const payload = { type: "notifications" };
 
-  const notifications = await fetchAllNotifications(bgozhURL, cookies, JSON.stringify(payload));
+  while (true) {
+    const notifications = await fetchAllNotifications(bgozhURL, cookies, JSON.stringify(payload));
+    if (notifications.length <= 0) {
+      console.log("No more notifications! Great Success 😎")
+      break
+    }
+    // Get the notificationIDs
+    console.log("NotificationIds: ");
+    let notificationIDs = notifications.flatMap((notification) => {
+      console.log(notification)
+      return notification.notificationIDs
+    })
 
-  // Get the notificationIDs
-  console.log("NotificationIds: ");
-  let notificationIDs = notifications.flatMap((notification) => {
-    console.log(notification)
-    return notification.notificationIDs
-  })
-
-  // Delete the notifications 
-  notificationIDs.forEach(id => {
-    const response = deleteNotification(bgozhURL, cookies, id);
-    console.log("response: " + response)
-  });
+    // Delete the notifications 
+    notificationIDs.forEach(id => {
+      const response = deleteNotification(bgozhURL, cookies, id);
+      console.log("response: " + response)
+    });
+  }
 }
+
